beb31fd87882ebb790a6fdb62048e297c8e77bf9
Colin Saliceti <c.saliceti@gmail.com>
2016-09-21 16:19:34 +0100

    Bosh terraform
    Create the AWS configuration required to install bosh. It includes
    security groups, key pairs, elastic IP and DNS, S3 bucket for blobstore
    and RDS database.
    
    See https://github.com/alphagov/paas-cf/tree/master/terraform for commit
    history.

---
+++ terraform/bosh/outputs.tf
@@ -0,0 +1,71 @@
+output "bosh_subnet_id" {
+  value = "${element(split(",", var.infra_subnet_ids), lookup(var.zone_index, var.bosh_az))}"
+}
+
+output "bosh_subnet_cidr" {
+  value = "${lookup(var.infra_cidrs, format("zone%s", lookup(var.zone_index, var.bosh_az)))}"
+}
+
+output "bosh_default_gw" {
+  value = "${lookup(var.infra_gws, lookup(var.infra_cidrs, format("zone%s", lookup(var.zone_index, var.bosh_az))))}"
+}
+
+output "microbosh_static_private_ip" {
+  value = "${lookup(var.microbosh_ips, var.bosh_az)}"
+}
+
+output "bosh_security_group" {
+  value = "${aws_security_group.bosh.name}"
+}
+
+output "bosh_managed_security_group" {
+  value = "${aws_security_group.bosh_managed.name}"
+}
+
+output "microbosh_static_public_ip" {
+  value = "${aws_eip.bosh.public_ip}"
+}
+
+output "compiled_cache_bucket_host" {
+  value = "s3-${var.region}.amazonaws.com"
+}
+
+output "compiled_cache_bucket_name" {
+  value = "shared-cf-bosh-blobstore-${var.aws_account}"
+}
+
+output "bosh_blobstore_bucket_name" {
+  value = "${aws_s3_bucket.bosh-blobstore.id}"
+}
+
+output "bosh_db_address" {
+  value = "${aws_db_instance.bosh.address}"
+}
+
+output "bosh_db_port" {
+  value = "${aws_db_instance.bosh.port}"
+}
+
+output "bosh_db_username" {
+  value = "${aws_db_instance.bosh.username}"
+}
+
+output "bosh_db_dbname" {
+  value = "${aws_db_instance.bosh.name}"
+}
+
+output "bosh_az" {
+  value = "${var.bosh_az}"
+}
+
+output "bosh_ssh_key_pair_name" {
+  value = "${aws_key_pair.bosh_ssh_key_pair.key_name}"
+}
+
+output "key_pair_name" {
+  value = "${aws_key_pair.env_key_pair.key_name}"
+}
+
+output "bosh_api_client_security_group" {
+  value = "${aws_security_group.bosh_api_client.name}"
+}
---
+++ terraform/bosh/variables.tf
@@ -0,0 +1,38 @@
+variable "secrets_bosh_postgres_password" {
+  description = "Master password for bosh database"
+}
+
+variable "bosh_db_multi_az" {
+  description = "BOSH database multi availabiliy zones"
+  default = "false"
+}
+
+variable "bosh_db_backup_retention_period" {
+  description = "BOSH database backup retention period"
+  default = "0"
+}
+
+variable "bosh_db_skip_final_snapshot" {
+  description = "Whether to skip final RDS snapshot (just before destroy). Differs per environment."
+  default = "true"
+}
+
+variable "bosh_az" {
+  description = "A zone used to provision bosh"
+}
+
+variable "system_dns_zone_id" {
+  description = "Amazon Route53 DNS zone identifier for the system components. Different per account."
+}
+
+variable "system_dns_zone_name" {
+  description = "Amazon Route53 DNS zone name for the provisioned environment."
+}
+
+variable "bosh_fqdn" {
+  description = "DNS record pointing to BOSH internal IP"
+}
+
+variable "bosh_fqdn_external" {
+  description = "DNS record pointing to BOSH external IP"
+}
