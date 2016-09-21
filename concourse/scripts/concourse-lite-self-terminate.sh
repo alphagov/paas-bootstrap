be952f705b3afc7d0b2ee31e2b8294115685b8b6
Colin Saliceti <c.saliceti@gmail.com>
2016-09-21 16:28:31 +0100

    Concourse scripts
    Utility scripts to configure environment variables, deploy pipelines,
    extract variables, login to concourse, login to bosh, etc.
    
    See https://github.com/alphagov/paas-cf/tree/master/concourse/scripts
    for commit history.

---
+++ concourse/scripts/concourse-lite-self-terminate.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -eu
+
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+
+"${SCRIPT_DIR}/fly_sync_and_login.sh"
+
+env=${DEPLOY_ENV}
+pipeline="self-terminate"
+config="${SCRIPT_DIR}/../pipelines/concourse-lite-self-terminate.yml"
+
+generate_vars_file() {
+   cat <<EOF
+---
+deploy_env: ${env}
+log_level: ${LOG_LEVEL:-}
+EOF
+}
+
+generate_vars_file > /dev/null # Check for missing vars
+
+bash "${SCRIPT_DIR}/deploy-pipeline.sh" \
+   "${env}" "${pipeline}" "${config}" <(generate_vars_file)
---
+++ concourse/scripts/pipelines.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -eu
+
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+
+"${SCRIPT_DIR}/fly_sync_and_login.sh"
+
+env=${DEPLOY_ENV}
+
+generate_vars_file() {
+   cat <<EOF
+---
+aws_account: ${AWS_ACCOUNT}
+vagrant_ip: ${VAGRANT_IP}
+deploy_env: ${env}
+state_bucket: gds-paas-${env}-bootstrap
+branch_name: ${BRANCH:-master}
+aws_region: ${AWS_DEFAULT_REGION:-eu-west-1}
+concourse_atc_password: ${CONCOURSE_ATC_PASSWORD}
+log_level: ${LOG_LEVEL:-}
+system_dns_zone_name: ${SYSTEM_DNS_ZONE_NAME}
+bosh_az: ${BOSH_AZ:-eu-west-1a}
+bosh_manifest_state: bosh-manifest-state-${BOSH_AZ:-eu-west-1a}.json
+bosh_fqdn: bosh.${SYSTEM_DNS_ZONE_NAME}
+bosh_fqdn_external: bosh-external.${SYSTEM_DNS_ZONE_NAME}
+concourse_atc_password: ${CONCOURSE_ATC_PASSWORD}
+EOF
+}
+
+generate_vars_file > /dev/null # Check for missing vars
+
+for ACTION in create destroy; do
+  bash "${SCRIPT_DIR}/deploy-pipeline.sh" \
+    "${env}" "${ACTION}" \
+    "${SCRIPT_DIR}/../pipelines/${ACTION}.yml" \
+    <(generate_vars_file)
+done
