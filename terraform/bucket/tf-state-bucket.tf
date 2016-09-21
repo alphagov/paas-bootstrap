42c3ef643a29f39d84bee7d7d57ed396f05eec6e
Colin Saliceti <c.saliceti@gmail.com>
2016-09-21 16:18:25 +0100

    S3 state bucket terraform
    Create the bucket that will receive bosh-init state, manifests, secrets,
    keys, etc used in the whole pipeline.
    
    See https://github.com/alphagov/paas-cf/tree/master/terraform for commit
    history.

---
+++ terraform/bucket/tf-state-bucket.tf
@@ -0,0 +1,17 @@
+variable "state_bucket" {
+  description = "Name of the AWS S3 bucket used to store the state"
+}
+
+provider "aws" {
+  region = "${var.region}"
+}
+
+resource "aws_s3_bucket" "terraform-state-s3" {
+  bucket = "${var.state_bucket}"
+  acl = "private"
+  force_destroy = "true"
+  versioning {
+    enabled = true
+  }
+}
+
