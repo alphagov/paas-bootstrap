052005e865692082502859bb2d85d7e069e91341
benhyland <ben.hyland@digital.cabinet-office.gov.uk>
2016-12-15 11:39:25 +0000

    Allow STS creds to be used to bootstrap
    We need to allow short-lived credentials to be used in order
    to require MFA for aws users.
    
    The sts-generated session token needs to be passed to vagrant.
    
    The concourse atc password can no longer be generated from
    aws creds, since they are no longer static but may change each day
    when short-lived credentials are regenerated.
    For the bootstrap pipeline, we can use a static atc password, since the
    only access is via ssh - the concourse is not exposed to the
    public internet. It is based on the AWS UserId so it is not
    predictable easily and is unique to each user.

--- manifests/concourse-manifest/spec/support/manifest_helpers.rb
+++ manifests/concourse-manifest/spec/support/manifest_helpers.rb
@@ -31,7 +31,6 @@ private
         File.expand_path("../../../../shared/build_manifest.sh", __FILE__),
         File.expand_path("../../../concourse-base.yml", __FILE__),
         File.expand_path("../../../extensions/datadog-agent.yml", __FILE__),
-        File.expand_path("../../fixtures/predefined-concourse-secrets.yml", __FILE__),
         File.expand_path("../../fixtures/generated-concourse-secrets.yml", __FILE__),
         File.expand_path("../../fixtures/concourse-terraform-outputs.yml", __FILE__),
         File.expand_path("../../fixtures/bosh-terraform-outputs.yml", __FILE__),
