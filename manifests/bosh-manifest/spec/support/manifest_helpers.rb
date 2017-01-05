04e6b1fee89fc29e803c912eb26622d6ce126ce6
Hector Rivas Gandara <keymon@gmail.com>
2017-01-05 17:14:33 +0000

    Rename bosh datadog addon files
    The datadog agent in bosh is deployed as a normal template, as the
    bosh-init does not support runtime-config.
    
    But it makes sense, for consistenct, call the stub for datadog `addon`

--- manifests/bosh-manifest/spec/support/manifest_helpers.rb
+++ manifests/bosh-manifest/spec/support/manifest_helpers.rb
@@ -35,11 +35,11 @@ private
       [
         File.expand_path("../../../../shared/build_manifest.sh", __FILE__),
         File.expand_path("../../../bosh-manifest.yml", __FILE__),
-        File.expand_path("../../../extensions/datadog-agent.yml", __FILE__),
         File.expand_path("../../fixtures/bosh-secrets.yml", __FILE__),
         File.expand_path("../../fixtures/bosh-ssl-certificates.yml", __FILE__),
         File.expand_path("../../fixtures/bosh-terraform-outputs.yml", __FILE__),
-        File.expand_path("../../../../shared/deployments/datadog-agent.yml", __FILE__),
+        File.expand_path("../../../addons/datadog-agent.yml", __FILE__),
+        File.expand_path("../../../../runtime-config/addons-meta/datadog-agent.yml", __FILE__),
       ].join(' ')
     )
     expect(status).to be_success, "build_manifest.sh exited #{status.exitstatus}, stderr:\n#{error}"
