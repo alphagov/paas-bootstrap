bcce6b039f7de65f76f44d799631c70e351ff54c
henrytk <henry.knott@digital.cabinet-office.gov.uk>
2018-01-23 13:43:13 +0000

    Delete DNS validation record upon cert deletion
    We do not want orphaned DNS records lingering after a certificate is
    deleted as it will be unclear which record is currently relevant.
    
    It would be possible to poll route53 to wait until the record is really
    gone, but I didn't do this for two reasons:
    
    * we have confidence that it successfully deleted the record from the
      return value of AWS CLI command
    * it would add potentially several minutes to the execution time

--- concourse/scripts/delete_concourse_cert.sh
+++ concourse/scripts/delete_concourse_cert.sh
@@ -11,5 +11,40 @@ if [ -z "${arn}" ] || [ "${arn}" = "None" ]; then
   exit 0
 fi
 
-echo "Deleting cert for ${concourse_fqdn} - arn: ${arn}"
+echo "Deleting DNS validation record for ${concourse_fqdn} - ARN: ${arn}"
+cert_info=$(aws acm describe-certificate --certificate-arn "${arn}" --query 'Certificate')
+dns_validation_record=$(echo "${cert_info}" | jq -r '.DomainValidationOptions[0].ResourceRecord.Name')
+dns_validation_value=$(echo "${cert_info}" | jq -r '.DomainValidationOptions[0].ResourceRecord.Value')
+
+if [ "null" = "${dns_validation_record}" ] || [ "null" = "${dns_validation_value}" ]; then
+  echo "Could not find DNS validation records for ${concourse_fqdn} - ARN: ${arn}"
+  exit 1
+fi
+
+get_route53_change_batch() {
+  action=${1}
+  cat <<EOF
+{
+  "Changes": [
+    {
+      "Action": "${action}",
+      "ResourceRecordSet": {
+        "Name": "${dns_validation_record}",
+        "Type": "CNAME",
+        "TTL": 300,
+        "ResourceRecords": [
+          {
+            "Value": "${dns_validation_value}"
+          }
+        ]
+      }
+    }
+  ]
+}
+EOF
+}
+
+aws route53 change-resource-record-sets --hosted-zone-id "${SYSTEM_DNS_ZONE_ID}" --change-batch "$(get_route53_change_batch DELETE)" > /dev/null
+
+echo "Deleting cert for ${concourse_fqdn} - ARN: ${arn}"
 aws acm delete-certificate --certificate-arn "${arn}"
