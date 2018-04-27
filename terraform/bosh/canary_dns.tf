255f11c8d0e79f672add7f70392a5f552f0342f3
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2018-04-27 12:03:10 +0100

    Use an A record for canary DNS entry
    The version of the datadog agent we're using doesn't support the
    record_type option.

--- terraform/bosh/canary_dns.tf
+++ terraform/bosh/canary_dns.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "canary" {
   zone_id = "${var.system_dns_zone_id}"
   name    = "__canary.${var.system_dns_zone_name}"
-  type    = "TXT"
+  type    = "A"
   ttl     = "1"
-  records = ["Test record for DNS resolution checks."]
+  records = ["127.0.0.1"]
 }
