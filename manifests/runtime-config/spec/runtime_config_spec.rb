e4904f31aa76f7cbf6cd1c9be5bc91b6e66e5b56
Michael Mokrysz <michael.mokrysz@digital.cabinet-office.gov.uk>
2018-05-11 16:51:09 +0100

    fixup! [WIP] Configure syslog_forwarder job for Logit

--- manifests/runtime-config/spec/runtime_config_spec.rb
+++ manifests/runtime-config/spec/runtime_config_spec.rb
@@ -35,12 +35,40 @@ RSpec.describe "Runtime config" do
       expect(syslog_forwarder_job).not_to be_nil
     end
 
-    it "has syslog_forwarder configured with a address based on the variable $SYSTEM_DNS_ZONE_NAME" do
+    it "has syslog_forwarder configured based on the variable $LOGIT_SYSLOG_ADDRESS" do
       syslog_forwarder_addon = runtime_config.fetch("addons").find { |addon| addon["name"] == "syslog_forwarder" }
       syslog_forwarder_address = syslog_forwarder_addon.fetch("properties").fetch("syslog").fetch("address")
 
-      expect(syslog_forwarder_address).to eq "logsearch-ingestor.#{ManifestHelpers::SYSTEM_DNS_ZONE_NAME}"
+      expect(syslog_forwarder_address).to eq ManifestHelpers::LOGIT_SYSLOG_ADDRESS
     end
+
+    it "has syslog_forwarder configured based on the variable $LOGIT_SYSLOG_PORT" do
+      syslog_forwarder_addon = runtime_config.fetch("addons").find { |addon| addon["name"] == "syslog_forwarder" }
+      syslog_forwarder_port = syslog_forwarder_addon.fetch("properties").fetch("syslog").fetch("port")
+
+      expect(syslog_forwarder_port).to eq ManifestHelpers::LOGIT_SYSLOG_PORT
+    end
+
+    # it "has syslog_forwarder configured with a address based on the variable $SYSTEM_DNS_ZONE_NAME" do
+    #   syslog_forwarder_addon = runtime_config.fetch("addons").find { |addon| addon["name"] == "syslog_forwarder" }
+    #   syslog_forwarder_address = syslog_forwarder_addon.fetch("properties").fetch("syslog").fetch("address")
+    #
+    #   expect(syslog_forwarder_address).to eq "logsearch-ingestor.#{ManifestHelpers::SYSTEM_DNS_ZONE_NAME}"
+    # end
+
+    it "has syslog_forwarder configured with a permitted_peer of *.logit.io" do
+      syslog_forwarder_addon = runtime_config.fetch("addons").find { |addon| addon["name"] == "syslog_forwarder" }
+      syslog_forwarder_permitted_peer = syslog_forwarder_addon.fetch("properties").fetch("syslog").fetch("permitted_peer")
+
+      expect(syslog_forwarder_permitted_peer).to eq "*.logit.io"
+    end
+
+    # it "has syslog_forwarder configured with a permitted_peer based on the variable $SYSTEM_DNS_ZONE_NAME" do
+    #   syslog_forwarder_addon = runtime_config.fetch("addons").find { |addon| addon["name"] == "syslog_forwarder" }
+    #   syslog_forwarder_permitted_peer = syslog_forwarder_addon.fetch("properties").fetch("syslog").fetch("permitted_peer")
+    #
+    #   expect(syslog_forwarder_permitted_peer).to eq "*.#{ManifestHelpers::SYSTEM_DNS_ZONE_NAME}"
+    # end
 
     it "has syslog_forwarder configured with tls enabled" do
       syslog_forwarder_addon = runtime_config.fetch("addons").find { |addon| addon["name"] == "syslog_forwarder" }
@@ -49,11 +77,32 @@ RSpec.describe "Runtime config" do
       expect(syslog_forwarder_tls_enabled).to be true
     end
 
-    it "has syslog_forwarder configured with a permitted_peer based on the variable $SYSTEM_DNS_ZONE_NAME" do
+    it "has syslog_forwarder configured based on the variable $LOGIT_CA_CERT" do
       syslog_forwarder_addon = runtime_config.fetch("addons").find { |addon| addon["name"] == "syslog_forwarder" }
-      syslog_forwarder_permitted_peer = syslog_forwarder_addon.fetch("properties").fetch("syslog").fetch("permitted_peer")
+      syslog_forwarder_port = syslog_forwarder_addon.fetch("properties").fetch("syslog").fetch("ca_cert")
 
-      expect(syslog_forwarder_permitted_peer).to eq "*.#{ManifestHelpers::SYSTEM_DNS_ZONE_NAME}"
+      expect(syslog_forwarder_port).to eq ManifestHelpers::LOGIT_CA_CERT
+    end
+
+    it "has syslog_forwarder configured with client tls enabled" do
+      syslog_forwarder_addon = runtime_config.fetch("addons").find { |addon| addon["name"] == "syslog_forwarder" }
+      syslog_forwarder_client_tls_enabled = syslog_forwarder_addon.fetch("properties").fetch("syslog").fetch("client_tls.enabled")
+
+      expect(syslog_forwarder_client_tls_enabled).to be true
+    end
+
+    it "has syslog_forwarder configured based on the variable $LOGIT_CLIENT_CERT" do
+      syslog_forwarder_addon = runtime_config.fetch("addons").find { |addon| addon["name"] == "syslog_forwarder" }
+      syslog_forwarder_client_tls_cert = syslog_forwarder_addon.fetch("properties").fetch("syslog").fetch("client_tls.cert")
+
+      expect(syslog_forwarder_client_tls_cert).to eq ManifestHelpers::LOGIT_CLIENT_CERT
+    end
+
+    it "has syslog_forwarder configured based on the variable $LOGIT_CLIENT_KEY" do
+      syslog_forwarder_addon = runtime_config.fetch("addons").find { |addon| addon["name"] == "syslog_forwarder" }
+      syslog_forwarder_client_tls_key = syslog_forwarder_addon.fetch("properties").fetch("syslog").fetch("client_tls.key")
+
+      expect(syslog_forwarder_client_tls_key).to eq ManifestHelpers::LOGIT_CLIENT_KEY
     end
   end
 end
