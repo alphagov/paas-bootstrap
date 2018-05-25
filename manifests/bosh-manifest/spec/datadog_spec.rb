4467f204dbd4763f11c191a65230b398c0528668
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2018-05-25 17:05:27 +0100

    Add datadog-agent to bosh job instead of using addons.
    bosh create-env doesn't support addons, so we have to add this to the
    job instead.

--- manifests/bosh-manifest/spec/datadog_spec.rb
+++ manifests/bosh-manifest/spec/datadog_spec.rb
@@ -1,18 +1,20 @@
 RSpec.describe "datadog addon config" do
   let(:manifest) { manifest_with_defaults }
-  let(:addon) { manifest.fetch("addons").find { |a| a['name'] == 'datadog-agent' } }
-  let(:addon_properties) { addon.fetch('jobs').find { |j| j['name'] == 'datadog-agent' }.fetch('properties') }
-
 
   let(:bosh_instance_group) { manifest.fetch("instance_groups").find { |x| x["name"] == "bosh" } }
   let(:bosh_properties) { bosh_instance_group.fetch('properties') }
 
-  it "configures the datadog-agent addon" do
-    expect(addon).to be
+  let(:datadog_job) { bosh_instance_group.fetch('jobs').find { |j| j['name'] == 'datadog-agent' } }
+  let(:datadog_properties) { datadog_job.fetch('properties') }
+
+  it "configures the datadog-agent job" do
+    expect(datadog_job).to be
+    expect(datadog_properties["enabled"]).to eq(true)
   end
 
-  it "configures datadog tag bosh-job" do
-    expect(addon_properties["tags"]["bosh-job"]).to eq("bosh")
+  it "configures the datadog tags" do
+    expect(datadog_properties["tags"]["bosh-job"]).to eq("bosh")
+    expect(datadog_properties["tags"]["deploy_env"]).to eq("test")
   end
 
   it "adds hm datadog properties" do
