6f263583921a6de61d5d0fc52a050658c3bbe928
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2018-05-30 11:05:00 +0100

    Update bosh/concourse to new instance_groups naming.
    These manifests were still using the old jobs/templates naming instead
    of the new instance_groups/jobs naming. This causes confusion when
    working between these manifests and the CF manifest.
    
    The new naming also makes much more sense because jobs in the manifest
    now reference the same thing as jobs within a boshrelease.
    
    The newer format also allows for properties as a job (formerly template)
    level, which will allow us to configure things like the datadog
    properties without the potwntial for clashes with other jobs properties.

--- manifests/bosh-manifest/spec/properties_spec.rb
+++ manifests/bosh-manifest/spec/properties_spec.rb
@@ -1,8 +1,8 @@
 RSpec.describe "manifest properties validations" do
   let(:manifest) { manifest_with_defaults }
   let(:properties) { manifest.fetch("properties") || Hash.new }
-  let(:bosh_job) { manifest.fetch("jobs").select { |x| x["name"] == "bosh" }.first }
-  let(:bosh_properties) { properties.merge(bosh_job["properties"]) }
+  let(:bosh_instance_group) { manifest.fetch("instance_groups").select { |x| x["name"] == "bosh" }.first }
+  let(:bosh_properties) { properties.merge(bosh_instance_group["properties"]) }
 
   it "configures hm bosh user with password" do
     users = bosh_properties["director"]["user_management"]["local"]["users"]
--- manifests/bosh-manifest/spec/shared_config_spec.rb
+++ manifests/bosh-manifest/spec/shared_config_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe "Gets properties from shared config" do
   let(:manifest) { manifest_with_defaults }
-  let(:bosh_properties) { manifest.fetch("jobs").select { |x| x["name"] == "bosh" }.first["properties"] }
+  let(:bosh_properties) { manifest.fetch("instance_groups").select { |x| x["name"] == "bosh" }.first["properties"] }
 
   it "for datadog" do
     expect(bosh_properties["use_dogstatsd"]).to eq false
