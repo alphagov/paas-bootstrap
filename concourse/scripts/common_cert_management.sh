6622a627bae74391e943f509af0f6bf1d263d95f
Hector Rivas Gandara <keymon@gmail.com>
2018-10-12 15:28:03 +0100

    Ensure the ACM DNS verification record is right
    Because some reason it happens that in some environments (specially
    prod) the DNS record to verify the certificate is wrong, pointing
    to a loadbalancer instead to the right verification value.
    
    To fix this automatically, we change the create_acm_cert.sh script
    to also verify if the value of the DNS is right, and update it
    if not.

--- concourse/scripts/common_cert_management.sh
+++ concourse/scripts/common_cert_management.sh
@@ -16,6 +16,15 @@ get_dns_validation_record() {
   dns_validation_value=$(echo "${cert_info}" | jq -r '.DomainValidationOptions[0].ResourceRecord.Value')
 }
 
+get_route53_resource_record_value() {
+  zone="$1"
+  name="$2"
+  aws route53 list-resource-record-sets \
+    --hosted-zone-id "${zone}" \
+    --query "ResourceRecordSets[?Name == '${name}'].ResourceRecords[0].Value" \
+    --output text
+}
+
 get_route53_change_batch() {
   action=${1}
   cat <<EOF
