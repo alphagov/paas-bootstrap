b9323815c2a0b32c61e8752ba9969a0380e9987f
bandesz <andras.ferencz-szabo@digital.cabinet-office.gov.uk>
2018-12-12 11:23:38 +0000

    Remove the Riemann exporter from Concourse
    Concourse can use only one metric emitter [1], so we have to disable Riemann to
    enable the Prometheus exporter.
    
    [1] https://github.com/concourse/atc/blob/f8bd990b2a64b380651ebb6c5999bbcad10de293/metric/emit.go#L73

--- manifests/concourse-manifest/spec/datadog_spec.rb
+++ manifests/concourse-manifest/spec/datadog_spec.rb
@@ -16,18 +16,4 @@ RSpec.describe "datadog agent config" do
   it "adds deploy_env from the terraform environment as a tag to datadog" do
     expect(datadog_job["properties"]["tags"]["deploy_env"]).to eq("test")
   end
-
-  describe "concourse-specific configuration" do
-    let(:concourse_instance_group) { manifest.fetch("instance_groups").find { |ig| ig["name"] == "concourse" } }
-    let(:riemann_job) { concourse_instance_group.fetch("jobs").find { |j| j["name"] == "riemann" } }
-    let(:atc_job) { concourse_instance_group.fetch("jobs").find { |j| j["name"] == "atc" } }
-
-    it "adds riemann" do
-      expect(riemann_job).to be
-    end
-
-    it "configures atc to export metrics to riemann" do
-      expect(atc_job["properties"]["riemann"]).to be
-    end
-  end
 end
