346f0516155f8ed7567db65781fcde30a688fd64
Hector Rivas Gandara <keymon@gmail.com>
2018-12-24 15:52:14 +0000

    bosh-deployment: migrate secrets to vars-store
    We are going to start using a vars-store.yml file and rely on the
    secret and certificate generation from `bosh interpolate`.
    
    But we want to keep the same old secrets and certs in the existing
    deployments. We add a script that would be able to populate
    a vars-store.yml file from `bosh_secrets.yml` and a directory
    with certificates.
    
    We temporary add the secrets to generate-bosh-secrets.rb so
    they do not get removed when we run bosh-generate-secrets.

--- manifests/bosh-manifest/scripts/generate-bosh-secrets.rb
+++ manifests/bosh-manifest/scripts/generate-bosh-secrets.rb
@@ -7,6 +7,16 @@ require File.expand_path("../../../shared/lib/secret_generator", __FILE__)
 generator = SecretGenerator.new(
   "bosh_postgres_password" => :simple,
   "bosh_vcap_password" => :sha512_crypted,
+  "bosh_mbus_bootstrap_password" => :simple,
+  "bosh_registry_password" => :simple,
+  "bosh_redis_password" => :simple,
+  "bosh_hm_director_password" => :simple,
+  "bosh_admin_password" => :simple,
+  "bosh_bosh_exporter_password" => :simple,
+  "bosh_uaa_postgres_password" => :simple,
+  "bosh_uaa_login_client_password" => :simple,
+  "bosh_uaa_uaa_encryption_key_1" => :simple,
+  "bosh_uaa_jwt_signing_key" => :bosh_rsa_key,
 )
 
 option_parser = OptionParser.new do |opts|
