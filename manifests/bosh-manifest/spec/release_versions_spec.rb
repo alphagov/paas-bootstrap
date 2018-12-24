4cb1b76dcd313131c4be2018edf524fae42e5041
Hector Rivas Gandara <keymon@gmail.com>
2018-12-24 15:35:41 +0000

    bosh-deployment: spec uses new scripts
    We want to test the manifest using the generated bosh deployment.
    
    We also:
    
     - Update travis to install bosh-cli and certstrap, required to
       run the script, and stop downloading spruce, not needed anymore.
     - Add additional tests to check the version of the bosh releases and
       that we are not accidentally pinning and old version of any.
       We copied the tests from paas-cf.
    
    To ensure that we check all the bosh-releases from upstream, we add
    a helper that renders the upstream manifest including all the
    opsfiles called `operations.d/*-UPSTREAM.yml`

---
+++ manifests/bosh-manifest/spec/release_versions_spec.rb
@@ -0,0 +1,66 @@
+require 'uri'
+
+RSpec.describe "release versions" do
+  matcher :match_version_from_url do |url|
+    match do |version|
+      if url =~ %r{\?v=(.+)\z}
+        url_version = $1
+      elsif url =~ %r{-([\d.]+)(-[0-9a-z-.]+)?\.tgz\z}
+        url_version = $1
+      else
+        raise "Failed to extract version from URL '#{url}'"
+      end
+      version == url_version
+    end
+  end
+
+  let(:manifest) { manifest_with_defaults }
+
+  specify "release versions match their download URL version" do
+    manifest.fetch("releases").each do |release|
+      expect(release.fetch('version')).to match_version_from_url(release.fetch('url')),
+        "expected release #{release['name']} version #{release['version']} to have matching version in URL: #{release['url']}"
+    end
+  end
+
+  specify "manifest versions are not older than the ones in bosh-deployment" do
+    def normalise_version(v)
+      Gem::Version.new(v.gsub(/^v/, '').gsub(/^([0-9]+)$/, '0.0.\1'))
+    end
+
+    # Versions to be pinned and corresponding upstream version
+    #  "example" => {
+    #    local: "1.1.1",
+    #    upstream: "2.2.2"
+    #  }
+    pinned_releases = {}
+
+    manifest_releases = manifest.fetch("releases").map { |release|
+      [release['name'], release['version']]
+    }.to_h
+
+    bosh_deployment_releases = bosh_deployment_manifest.fetch("releases").map { |release|
+      [release['name'], release['version']]
+    }.to_h
+
+    unpinned_bosh_deployment_releases = bosh_deployment_releases.reject { |name, _version|
+      pinned_releases.has_key? name
+    }.to_h
+
+    unpinned_bosh_deployment_releases.each { |name, version|
+      next unless manifest_releases.has_key? name
+      expect(normalise_version(manifest_releases[name])).to be >= normalise_version(version),
+        "expected #{name} release version #{manifest_releases[name]} to be older than #{version} as defined in bosh-deployment. Maybe you need to pin it?"
+    }
+
+    pinned_releases.each { |name, pinned_versions|
+      expect(manifest_releases).to have_key(name), "expected pinned release #{name} for found in manifest"
+      expect(bosh_deployment_releases).to have_key(name), "expected pinned release #{name} for found in bosh-deployment"
+      expect(normalise_version(manifest_releases[name])).to be(normalise_version(pinned_versions[:local])),
+         "expected #{name} to be using our own built tarball #{pinned_versions[:local]} not #{manifest_releases[name]}"
+
+      expect(normalise_version(bosh_deployment_releases[name])).to be(normalise_version(pinned_versions[:upstream])),
+        "expected #{name} upstream to be #{pinned_versions[:upstream]} not #{bosh_deployment_releases[name]}. We might need to rebase our forked #{name} release and generate a new tarball, or use the upstream version."
+    }
+  end
+end
--- manifests/bosh-manifest/spec/support/manifest_helpers.rb
+++ manifests/bosh-manifest/spec/support/manifest_helpers.rb
@@ -2,6 +2,8 @@ require 'singleton'
 require 'open3'
 require 'yaml'
 require 'tempfile'
+require 'fileutils'
+
 
 module ManifestHelpers
   SYSTEM_DNS_ZONE_NAME = 'example.com'.freeze
@@ -9,12 +11,17 @@ module ManifestHelpers
   class Cache
     include Singleton
     attr_accessor :manifest_with_defaults
+    attr_accessor :bosh_deployment_manifest
     attr_accessor :bosh_secrets_file
     attr_accessor :bosh_secrets_data
   end
 
   def manifest_with_defaults
     Cache.instance.manifest_with_defaults ||= load_default_manifest
+  end
+
+  def bosh_deployment_manifest
+    Cache.instance.bosh_deployment_manifest ||= load_bosh_deployment_with_upstream_opsfiles
   end
 
   def bosh_secrets_file
@@ -33,29 +40,62 @@ module ManifestHelpers
 
 private
 
+  def root
+    Pathname(File.expand_path("../../../..", __dir__))
+  end
+
+  def vars_store_file
+    @vars_store_file ||= Tempfile.open(['vars-store', '.yml'])
+    Pathname(@vars_store_file)
+  end
+
+  def workdir
+    @workdir ||= Dir.mktmpdir("workdir")
+    Pathname(@workdir)
+  end
+
   def fake_env_vars
-    ENV["BOSH_FQDN_EXTERNAL"] = "bosh-external.domain"
-    ENV["BOSH_FQDN"] = "bosh.domain"
-    ENV["AWS_ACCOUNT"] = "dev"
-    ENV["BOSH_INSTANCE_PROFILE"] = "bosh-director-build"
-    ENV["DEPLOY_ENV"] = ManifestHelpers.deploy_env
-    ENV["SYSTEM_DNS_ZONE_NAME"] = ManifestHelpers::SYSTEM_DNS_ZONE_NAME
+    env = {}
+    env["BOSH_FQDN_EXTERNAL"] = "bosh-external.domain"
+    env["BOSH_FQDN"] = "bosh.domain"
+    env["AWS_ACCOUNT"] = "dev"
+    env["AWS_DEFAULT_REGION"] = "eu-west-1"
+    env["BOSH_INSTANCE_PROFILE"] = "bosh-director-build"
+    env["DEPLOY_ENV"] = ManifestHelpers.deploy_env
+    env["SYSTEM_DNS_ZONE_NAME"] = ManifestHelpers::SYSTEM_DNS_ZONE_NAME
+    env
   end
 
   def load_default_manifest
-    fake_env_vars
+    env = fake_env_vars
+
+    env['VARS_STORE'] = vars_store_file.to_s
+    env['PAAS_BOOTSTRAP_DIR'] = root.to_s
+    env['WORKDIR'] = workdir.to_s
+
+    generate_bosh_secrets
+    generate_bosh_ca_certs
+    copy_terraform_outputs
+
     output, error, status = Open3.capture3(
-      [
-        File.expand_path("../../../../shared/build_manifest.sh", __FILE__),
-        File.expand_path("../../../bosh-manifest.yml", __FILE__),
-        File.expand_path("../../../eu-west-1.yml", __FILE__),
-        bosh_secrets_file,
-        File.expand_path("../../fixtures/bosh-ssl-certificates.yml", __FILE__),
-        File.expand_path("../../../../shared/spec/fixtures/bosh-terraform-outputs.yml", __FILE__),
-        File.expand_path("../../../../shared/spec/fixtures/vpc-terraform-outputs.yml", __FILE__),
-      ].join(' ')
+      env,
+      root.join("manifests/bosh-manifest/scripts/generate-manifest.sh").to_s,
     )
-    expect(status).to be_success, "build_manifest.sh exited #{status.exitstatus}, stderr:\n#{error}"
+    expect(status).to be_success, "generate-manifests.sh exited #{status.exitstatus}, stderr:\n#{error}"
+
+    # Deep freeze the object so that it's safe to use across multiple examples
+    # without risk of state leaking.
+    deep_freeze(YAML.safe_load(output))
+  end
+
+  def load_bosh_deployment_with_upstream_opsfiles
+    upstream_ops_file = Dir.glob(root.join('manifests/bosh-manifest/operations.d/*-UPSTREAM.yml')).sort.map { |x| "--ops-file=#{x}" }
+    base_manifest_file = root.join('manifests/bosh-manifest/upstream/bosh.yml')
+
+    output, error, status = Open3.capture3(
+      "bosh interpolate #{upstream_ops_file.join(' ')} #{base_manifest_file}"
+    )
+    expect(status).to be_success, "generate bosh-deployment manifest with upstream opsfile failed with #{status.exitstatus}, stderr:\n#{error}"
 
     # Deep freeze the object so that it's safe to use across multiple examples
     # without risk of state leaking.
@@ -63,15 +103,41 @@ private
   end
 
   def generate_bosh_secrets
-    file = Tempfile.new(['test-bosh-secrets', '.yml'])
+    FileUtils.mkdir_p workdir.join('bosh-secrets').to_s
+    filename = workdir.join('bosh-secrets/bosh-secrets.yml').to_s
+    file = File.open(filename, "w")
     output, error, status = Open3.capture3(File.expand_path("../../../scripts/generate-bosh-secrets.rb", __FILE__))
     unless status.success?
       raise "Error generating bosh-secrets, exit: #{status.exitstatus}, output:\n#{output}\n#{error}"
     end
     file.write(output)
-    file.flush
-    file.rewind
+    file.close
     file
+  end
+
+  def generate_bosh_ca_certs
+    output, error, status = Open3.capture3(
+      "bash", "-e", "-c",
+      '
+        certstrap init --years "10" --passphrase "" --common-name bosh-CA
+        mkdir -p certs
+        mv out/* certs
+      ',
+      chdir: workdir.to_s,
+    )
+    unless status.success?
+      raise "Error generating bosh-secrets, exit: #{status.exitstatus}, output:\n#{output}\n#{error}"
+    end
+  end
+
+  def copy_terraform_outputs
+    FileUtils.mkdir_p workdir.join('terraform-outputs')
+    FileUtils.cp \
+      root.join('manifests/shared/spec/fixtures/vpc-terraform-outputs.yml').to_s,
+      workdir.join('terraform-outputs/vpc.terraform-outputs.yml')
+    FileUtils.cp \
+      root.join('manifests/shared/spec/fixtures/bosh-terraform-outputs.yml').to_s,
+      workdir.join('terraform-outputs/bosh.terraform-outputs.yml')
   end
 
   def deep_freeze(object)
