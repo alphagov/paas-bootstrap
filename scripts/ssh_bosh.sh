c9a924f65683c406e746eb6472f13227675c4671
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2019-02-01 14:00:03 +0000

    Stop using separate ssh keypair for bosh VM.
    There's no need for this to have a separate key pair - it's just one
    more thing for the pipeline to manage, and one extra point of
    complexity.
    
    This removes the bosh-specific keypair, and makes bosh use the default
    keypair.

--- scripts/ssh_bosh.sh
+++ scripts/ssh_bosh.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -euo pipefail
 
-BOSH_KEY=/tmp/bosh_id_rsa.$RANDOM
+BOSH_KEY=/tmp/id_rsa.$RANDOM
 BOSH_IP=$(aws ec2 describe-instances \
-    --filters "Name=key-name,Values=${DEPLOY_ENV}_bosh_ssh_key_pair" \
+    --filters "Name=tag:deploy_env,Values=${DEPLOY_ENV}" 'Name=tag:instance_group,Values=bosh' \
     --query 'Reservations[].Instances[].PublicIpAddress' --output text)
 
 trap 'rm -f ${BOSH_KEY}' EXIT
 
-aws s3 cp "s3://gds-paas-${DEPLOY_ENV}-state/bosh_id_rsa" ${BOSH_KEY} && chmod 400 ${BOSH_KEY}
+aws s3 cp "s3://gds-paas-${DEPLOY_ENV}-state/id_rsa" ${BOSH_KEY} && chmod 400 ${BOSH_KEY}
 
 aws s3 cp "s3://gds-paas-${DEPLOY_ENV}-state/bosh-secrets.yml" - | \
   ruby -ryaml -e 'puts "Sudo password is " + YAML.load(STDIN)["secrets"]["vcap_password_orig"]'
