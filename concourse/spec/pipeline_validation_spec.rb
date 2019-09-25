42d0815f5ea7b9ce0b0874e17174ec14007f156d
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2019-09-25 13:18:54 +0100

    Do not test concourse secret generation
    We have a test for what free variables are allowed in the pipeline, we
    need to add concourse_web_password to the list of allowed variables
    
    We use BOSH to generate and store the concourse secrets, so we do not
    need the ruby code that 1) generates secrets 2) tests that secret
    generation works
    
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- concourse/spec/pipeline_validation_spec.rb
+++ concourse/spec/pipeline_validation_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe 'pipeline validations' do
         .flatten
         .uniq
     )
-    .concat(%w[vagrant_ssh_key_name test-secret])
+    .concat(%w[vagrant_ssh_key_name test-secret concourse_web_password])
 
   it 'should have pipeline variables' do
     expect(ALL_PIPELINE_VARS.length).to_not eq(0)
