7cb41a6a70c33e9115c4611c3a23f26cab81ae0a
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2019-10-01 19:14:05 +0100

    Remove Concourse manifest tests which ref v1 props
    Concourse is no longer using v1 manifest properties, so we should not
    have tests for them.
    
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- manifests/concourse-manifest/spec/manifest_validation_spec.rb
+++ manifests/concourse-manifest/spec/manifest_validation_spec.rb
@@ -4,11 +4,8 @@ RSpec.describe "generic manifest validations" do
 
   describe "name uniqueness" do
     %w(
-      disk_pools
       instance_groups
-      networks
       releases
-      resource_pools
     ).each do |resource_type|
       specify "all #{resource_type} have a unique name" do
         all_resource_names = manifest.fetch(resource_type, []).map { |r| r["name"] }
@@ -46,15 +43,6 @@ RSpec.describe "generic manifest validations" do
   end
 
   describe "instance_group cross-references" do
-    specify "all instance_groups reference resource_pools that exist" do
-      resource_pool_names = manifest["resource_pools"].map { |r| r["name"] }
-
-      manifest["instance_groups"].each do |ig|
-        expect(resource_pool_names).to include(ig["resource_pool"]),
-          "resource_pool #{ig['resource_pool']} not found for instance_group #{ig['name']}"
-      end
-    end
-
     specify "all instance_group jobs reference releases that exist" do
       release_names = manifest["releases"].map { |r| r["name"] }
 
@@ -63,39 +51,6 @@ RSpec.describe "generic manifest validations" do
           expect(release_names).to include(job["release"]),
             "release #{job['release']} not found for job #{job['name']} in instance_group #{ig['name']}"
         end
-      end
-    end
-
-    specify "all instance_groups reference networks that exist" do
-      network_names = manifest["networks"].map { |n| n["name"] }
-
-      manifest["instance_groups"].each do |ig|
-        ig["networks"].each do |network|
-          expect(network_names).to include(network["name"]),
-            "network #{network['name']} not found for instance_group #{ig['name']}"
-        end
-      end
-    end
-
-    specify "all instance_groups reference disk_pools that exist" do
-      disk_pool_names = manifest.fetch("disk_pools", {}).map { |p| p["name"] }
-
-      manifest["instance_groups"].each do |ig|
-        next unless ig["persistent_disk_pool"]
-
-        expect(disk_pool_names).to include(ig["persistent_disk_pool"]),
-          "disk_pool #{ig['persistent_disk_pool']} not found for instance_group #{ig['name']}"
-      end
-    end
-  end
-
-  describe "resource_pools cross-references" do
-    specify "all resource_pools reference networks that exist" do
-      network_names = manifest["networks"].map { |n| n["name"] }
-
-      manifest["resource_pools"].each do |pool|
-        expect(network_names).to include(pool["network"]),
-          "network #{pool['network']} not found for resource_pool #{pool['name']}"
       end
     end
   end
