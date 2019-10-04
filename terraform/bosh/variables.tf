f7a2dca525029b1b6495e1e788633f5b230b512a
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2019-10-04 09:35:03 +0100

    [dev] make bosh rds multi-az always
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- terraform/bosh/variables.tf
+++ terraform/bosh/variables.tf
@@ -4,7 +4,7 @@ variable "secrets_bosh_postgres_password" {
 
 variable "bosh_db_multi_az" {
   description = "BOSH database multi availabiliy zones"
-  default     = "false"
+  default     = "true"
 }
 
 variable "bosh_db_backup_retention_period" {
