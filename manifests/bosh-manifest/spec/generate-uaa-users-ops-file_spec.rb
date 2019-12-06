c9e2c9da0b65c3fd83420be45bf8e50abf0f8239
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2019-12-06 15:48:53 +0000

    Create UAA (bosh) users for paas-trusted-people
    The file in S3 will be uploaded by the `make ENV upload-paas-trusted-people`
    make target.
    
    We're generating an ops file which will ultimately add these users to:
    
    ```
    /var/vcap/jobs/uaa/config/uaa.yml
    ```
    
    Which is UAA's config file on the bosh director.
    
    Using an ops file is nice because we don't have to think about removing
    users who shouldn't have access (if we did this using UAA's API then it
    would be more complicated).

---
+++ manifests/bosh-manifest/spec/generate-uaa-users-ops-file_spec.rb
@@ -0,0 +1,67 @@
+require 'tempfile'
+require 'yaml'
+
+require_relative '../scripts/generate-uaa-users-ops-file'
+
+def with_temp_users_file(contents)
+  Tempfile.create('users.yml') do |f|
+    f.write contents
+    f.flush
+    yield f
+  end
+end
+
+RSpec.describe 'Generating UAA users ops file' do
+  it 'Generates an empty ops file if no users are present' do
+    with_temp_users_file 'users: []' do |f|
+      result_str = generate_uaa_users_ops_file(f.path, 'some-aws-env')
+      result_yaml = YAML.safe_load(result_str)
+      expect(result_yaml).to eql([{
+        'type' => 'replace',
+        'path' => '/instance_groups/name=bosh/jobs/name=uaa/properties/uaa/scim/users',
+        'value' => [],
+      }])
+    end
+  end
+
+  it 'Generates an ops file with bosh-admins for the current environment based on the config' do
+    users = {
+      'users' => [
+        {
+          'email' => 'some-admin-email@digital.cabinet-office.gov.uk',
+          'roles' => { 'prod' => [{ 'role' => 'bosh-admin' }] },
+        },
+        {
+          'email' => 'some-admin-for-a-different-env-email@digital.cabinet-office.gov.uk',
+          'roles' => { 'dev' => [{ 'role' => 'bosh-admin' }] },
+        },
+        {
+          'email' => 'some-unrelated-roles-email@digital.cabinet-office.gov.uk',
+          'roles' => { 'prod' => [{ 'role' => 'some-unrelated-role' }] },
+        },
+        {
+          'email' => 'some-no-roles-email@digital.cabinet-office.gov.uk',
+          'roles' => { 'prod' => [] },
+        },
+        {
+          'email' => 'some-empty-roles-email@digital.cabinet-office.gov.uk',
+          'roles' => {},
+        },
+      ],
+    }
+    with_temp_users_file users.to_yaml do |f|
+      result_str = generate_uaa_users_ops_file(f.path, 'prod')
+      result_yaml = YAML.safe_load(result_str)
+      expect(result_yaml).to eql([{
+        'type' => 'replace',
+        'path' => '/instance_groups/name=bosh/jobs/name=uaa/properties/uaa/scim/users',
+        'value' => [{
+          'email' => 'some-admin-email@digital.cabinet-office.gov.uk',
+          'name' => 'some-admin-email@digital.cabinet-office.gov.uk',
+          'origin' => 'google',
+          'groups' => ['bosh.admin'],
+        }],
+      }])
+    end
+  end
+end
