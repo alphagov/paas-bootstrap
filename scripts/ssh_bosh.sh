de1074b4cb98f32f54536e16c7d6732a5740104c
Michael Mokrysz <michael.mokrysz@digital.cabinet-office.gov.uk>
2020-01-29 14:36:25 +0000

    Use default SSH key when SSHing to BOSH
    Some of the team use SSH keys on disk to access BOSH, but others
    use keys on a Yubikey. The existing script didn't support using a
    Yubikey.
    
    This commit removes all specifying of the SSH key, relying on the
    default. It also removes `-o IdentitiesOnly=yes` which was obstructing
    Yubikey use.

--- scripts/ssh_bosh.sh
+++ scripts/ssh_bosh.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
-SSH_PATH=${SSH_PATH:-"/Users/${USER}/.ssh/id_rsa"}
-
-BOSH_KEY=/tmp/id_rsa.$RANDOM
 BOSH_IP=$(aws ec2 describe-instances \
     --filters "Name=tag:deploy_env,Values=${DEPLOY_ENV}" 'Name=tag:instance_group,Values=bosh' \
     --query 'Reservations[].Instances[].PublicIpAddress' --output text)
-
-trap 'rm -f ${BOSH_KEY}' EXIT
-
-aws s3 cp "s3://gds-paas-${DEPLOY_ENV}-state/id_rsa" ${BOSH_KEY} && chmod 400 ${BOSH_KEY}
 
 aws s3 cp "s3://gds-paas-${DEPLOY_ENV}-state/bosh-secrets.yml" - | \
   ruby -ryaml -e 'puts "Sudo password is " + YAML.load(STDIN)["secrets"]["vcap_password_orig"]'
@@ -18,8 +11,6 @@ echo
 
 # shellcheck disable=SC2029
 ssh \
-    -i "$SSH_PATH" \
-    -o IdentitiesOnly=yes \
     -o ServerAliveInterval=60 \
     -o StrictHostKeyChecking=no \
     -o UserKnownHostsFile=/dev/null \
