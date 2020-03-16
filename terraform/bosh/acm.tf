a76cf8e1c0824daaded590144b384bbc4a2e50f5
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2020-03-16 16:49:24 +0000

    Add bosh load balancer forwarding to uaa
    This is so we can get an ACM cert for UAA externally
    
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- terraform/bosh/acm.tf
+++ terraform/bosh/acm.tf
@@ -1,9 +1,18 @@
+locals {
+  bosh_lb_domains = [
+    "bosh-external.${var.system_dns_zone_name}",
+    "bosh-uaa-external.${var.system_dns_zone_name}",
+  ]
+
+  bosh_lb_domain_name = "${element(local.bosh_lb_domains, 0)}"
+
+  bosh_lb_sans = ["${slice(local.bosh_lb_domains, 1, length(local.bosh_lb_domains))}"]
+}
+
 resource "aws_acm_certificate" "bosh" {
   domain_name = "bosh-external.${var.system_dns_zone_name}"
 
-  subject_alternative_names = [
-    "uaa-external.${var.system_dns_zone_name}",
-  ]
+  subject_alternative_names = ["${local.bosh_lb_sans}"]
 
   validation_method = "DNS"
 
@@ -13,7 +22,7 @@ resource "aws_acm_certificate" "bosh" {
 }
 
 resource "aws_route53_record" "bosh_cert_validation" {
-  count   = "${length(aws_acm_certificate.bosh.domain_validation_options)}"
+  count   = "${length(local.bosh_lb_domains)}"
   zone_id = "${var.system_dns_zone_id}"
   ttl     = 60
 
@@ -31,6 +40,8 @@ resource "aws_route53_record" "bosh_cert_validation" {
     aws_acm_certificate.bosh.domain_validation_options[count.index],
     "resource_record_value"
   )}"]
+
+  depends_on = ["aws_acm_certificate.bosh"]
 }
 
 resource "aws_acm_certificate_validation" "bosh" {
---
+++ terraform/bosh/lb.tf
@@ -0,0 +1,98 @@
+resource "aws_security_group" "bosh_lb" {
+  name        = "${var.env}-bosh-lb"
+  description = "BOSH LB security group"
+  vpc_id      = "${var.vpc_id}"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["${var.admin_cidrs}"]
+  }
+
+  tags {
+    Name = "${var.env}-bosh-lb"
+  }
+}
+
+resource "aws_lb" "bosh" {
+  name               = "${var.env}-bosh"
+  idle_timeout       = 600
+  load_balancer_type = "application"
+
+  subnets = ["${split(",", var.infra_subnet_ids)}"]
+
+  security_groups = [
+    "${aws_security_group.bosh_api_client.id}",
+    "${aws_security_group.bosh_lb.id}",
+  ]
+
+  tags {
+    Name = "${var.env}-bosh-lb"
+  }
+}
+
+resource "aws_lb_listener" "bosh_tls" {
+  load_balancer_arn = "${aws_lb.bosh.arn}"
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
+  certificate_arn   = "${aws_acm_certificate.bosh.arn}"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Target not found"
+      status_code  = "404"
+    }
+  }
+}
+
+resource "aws_lb_target_group" "bosh_uaa" {
+  name        = "${var.env}-bosh-uaa"
+  port        = 8443
+  protocol    = "HTTPS"
+  target_type = "ip"
+  vpc_id      = "${var.vpc_id}"
+}
+
+resource "aws_lb_target_group_attachment" "bosh_uaa_bosh_static" {
+  target_group_arn = "${aws_lb_target_group.bosh_uaa.arn}"
+  target_id        = "${var.microbosh_static_private_ip}"
+}
+
+resource "aws_lb_listener_rule" "bosh_uaa" {
+  listener_arn = "${aws_lb_listener.bosh_tls.arn}"
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.bosh_uaa.arn}"
+  }
+
+  condition {
+    field  = "host-header"
+    values = ["bosh-uaa-external.${var.system_dns_zone_name}"]
+  }
+}
+
+resource "aws_route53_record" "bosh_uaa_external" {
+  zone_id = "${var.system_dns_zone_id}"
+  name    = "bosh-uaa-external.${var.system_dns_zone_name}"
+  type    = "A"
+
+  alias {
+    name                   = "${aws_lb.bosh.dns_name}"
+    zone_id                = "${aws_lb.bosh.zone_id}"
+    evaluate_target_health = false
+  }
+}
