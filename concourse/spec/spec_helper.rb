e3a0aea93fe7e27b94f65dae2cebbc172a269f6c
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2020-10-13 14:56:32 +0100

    concourse: add tests for resources
    copied from identical paas-cf
    
    our resources used for destroy have been reaped by docker hub
    
    seems like a good time to ensure all our resources are the same
    
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- concourse/spec/spec_helper.rb
+++ concourse/spec/spec_helper.rb
@@ -90,3 +90,33 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 end
+
+SPEC_DIR = File.expand_path(__dir__)
+CONCOURSE_DIR = File.expand_path(File.join(SPEC_DIR, ".."))
+TASKS_DIR = File.join(CONCOURSE_DIR, "tasks")
+PIPELINES_DIR = File.join(CONCOURSE_DIR, "pipelines")
+
+def concourse_tasks
+  Dir
+    .glob(File.join(TASKS_DIR, "*.yml"))
+    .map { |f| [File.basename(f), File.read(f)] }
+end
+
+def concourse_pipelines
+  Dir
+    .glob(File.join(PIPELINES_DIR, "*.yml"))
+    .map { |f| [File.basename(f), File.read(f)] }
+end
+
+def all_image_resources(frag)
+  if frag.is_a?(Array)
+    frag.flat_map { |val| all_image_resources(val) }
+  elsif !frag.is_a?(Hash)
+    []
+  elsif [frag.dig("source", "repository"), frag.dig("source", "tag")].none?
+    frag.values.flat_map { |val| all_image_resources(val) }
+  else
+    [{ repository: frag.dig("source", "repository"),
+       tag: frag.dig("source", "tag") }]
+  end
+end
