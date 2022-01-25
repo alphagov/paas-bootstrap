3ecd70590f82a1a12782112058742357e32b5800
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2022-01-25 13:18:35 +0000

    Automatically apply all ops files in Concourse manifest

--- manifests/concourse-manifest/scripts/generate-manifest.sh
+++ manifests/concourse-manifest/scripts/generate-manifest.sh
@@ -13,14 +13,19 @@ if [ "${ENABLE_GITHUB}" = "true" ] ; then
   fi
 fi
 
+opsfile_args=""
+
+for i in "${PAAS_BOOTSTRAP_DIR}"/manifests/concourse-manifest/operations.d/*.yml; do
+  opsfile_args="$opsfile_args $i"
+done
+
 # shellcheck disable=SC2086
 spruce merge \
   --prune meta \
   --prune secrets \
   --prune terraform_outputs \
   "${PAAS_BOOTSTRAP_DIR}/manifests/concourse-manifest/concourse-base.yml" \
-  "${PAAS_BOOTSTRAP_DIR}/manifests/concourse-manifest/operations.d/010-x-frame-options.yml" \
-  "${PAAS_BOOTSTRAP_DIR}/manifests/concourse-manifest/operations.d/400-audit.yml" \
+  ${opsfile_args} \
   "${WORKDIR}/bosh-secrets/bosh-secrets.yml" \
   "${WORKDIR}/terraform-outputs/concourse-terraform-outputs.yml" \
   "${WORKDIR}/terraform-outputs/vpc-terraform-outputs.yml" \
